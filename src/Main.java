c7ceb33960decea4772e2fde0a7e172780dfa87d
Ksenia Kozyreva <oestara91@gmail.com>
2020-05-07 17:54:34 +0300

    reduced the number of decimal places in ibm

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(70, 1.64);
-        System.out.println(bmi);
+        double bmi = service.calculate(69, 1.64);
+        System.out.printf("Ваш индекс массы тела - %.1f",bmi);
     }
 }
